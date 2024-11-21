7048c57acaed22557e31715f8bea6ff73a4f6f7a
Holly <162161116+Holl4444@users.noreply.github.com>
2024-11-21 15:09:54 +0000

    Change operate to take array. html struct css styl
    Changed calculation function to take an array to
    simplify linking to js later.
    
    Added structure and first round of styling to UI.
    Added fonts Roboto and Digit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function operate(num1, operator, num2) {
-    const op = operator;
+function operate(arr) {
+    const op = arr[1];
+    const num1 = arr[0];
+    const num2 = arr[2];
     const methods = {
         '+': () => {
             return num1 + num2;},
@@ -11,10 +13,10 @@ function operate(num1, operator, num2) {
             return num1 / num2;},
         '**': () => {
             return num1 ** num2;},
-        '{\displaystyle {\sqrt {x}},}': () => {
+        '√': () => {
             return Math.sqrt(num1)},
     };
    return methods[op]();
 }
 
-console.log(operate(36, '{\displaystyle {\sqrt {x}},}', 2));
+console.log(operate([36, '√', 2]));
